8bc925e3caffeae45ed33a7119607fc271d10a36
asudau <sudau@elan-ev.de>
2018-04-25 14:40:49 +0200

    anpassung jobstarter header

--- css/jobstarter_header.js
+++ css/jobstarter_header.js
@@ -5,7 +5,7 @@
  */
 
 $( document ).ready(function() {
-    document.getElementById("flex-header").innerHTML = "<div style='position:relative;width: 100%;padding-bottom: 16%;'><iframe id='logo-iframe' style='position:absolute;width:100%;height:100%;' src='' frameborder='0'></iframe></div>";
+    document.getElementById("flex-header").innerHTML = "<div style='position:relative;width: 50%;padding-bottom: 16%;'><iframe id='logo-iframe' style='position:absolute;width:100%;height:100%;' src='' frameborder='0'></iframe></div>";
     document.getElementById('logo-iframe').src = 'https://www.jobstarter.de/media/logos/horizontal_neu.html';
-    document.getElementById("flex-header").style.height='250px';
+    document.getElementById("flex-header").style.height='125px';
 });
