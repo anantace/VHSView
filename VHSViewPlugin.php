6a823d0fc36a33198f5901eba0ed48f368975b56
Annelene Sudau <sudau@elan-ev.de>
2021-10-06 14:04:13 +0200

    Aenderungswunsch Hauptnavigation

--- VHSViewPlugin.php
+++ VHSViewPlugin.php
@@ -7,35 +7,9 @@ class VHSViewPlugin extends StudipPlugin implements SystemPlugin
 	public function __construct() {
 	
 	    parent::__construct();
-	
-	    PageLayout::addStylesheet($this->getPluginURL() . '/assets/courseware.css');
-	    PageLayout::addScript($this->getPluginURL() . '/assets/script.js');
 		
 		global $auth, $perm, $user;
 		$username = Request::get('username', $auth->auth['uname']);
-
-		
-
-	 $course_id = $_SESSION['SessSemName'][1];
-
-
-	 if (Navigation::hasItem('/course/mooc_courseware')){
-			$query = "SELECT content AS value
-			FROM datafields_entries
-			WHERE datafield_id = 'c7f844e04a4c354e1db9a9e78859e58c'
-			AND range_id = '" . $course_id . "'
-			AND content = '1'";
-                            
-			$statement = DBManager::get()->prepare($query);
-			$statement->execute();
-			$courseware = $statement->fetchAll(PDO::FETCH_ASSOC);
-			
-			if (count($courseware)){
-				Navigation::removeItem('/course/mooc_courseware');
-			}
-	}
-
-	//Navigation::getItem('/search')->setTitle($course_id);
 
 
 		if (!$perm->have_perm('admin') && $user->id != 'nobody') {
@@ -83,40 +57,6 @@ class VHSViewPlugin extends StudipPlugin implements SystemPlugin
 				Navigation::removeItem('/start/search');
 			}
 			
-			if (Navigation::hasItem('/browse')){
-				
-				
-				$stmt = DBManager::get()->prepare("SELECT su.seminar_id FROM seminar_user su
-					WHERE su.user_id = ?");
-				$stmt->execute(array($GLOBALS['user']->id));
-				$count = $stmt->rowCount();
-				if($count == 1){
-					$result = $stmt->fetch();
-					Navigation::getItem('/browse')->setURL("/seminar_main.php?auswahl=". $result['seminar_id']);
-					Navigation::getItem('/browse')->setTitle("Mein Kurs");	
-				}
-				if($count == 0 && !$perm->have_perm('dozent')){
-					Navigation::removeItem('/browse');	
-				}
-				
-			}
-			
-			if (Navigation::hasItem('/course')){
-			
-			
-				$stmt = DBManager::get()->prepare("SELECT su.seminar_id FROM seminar_user su
-					WHERE su.user_id = ?");
-				$stmt->execute(array($GLOBALS['user']->id));
-				
-				$count = $stmt->rowCount();
-				if($count == 1){
-					$result = $stmt->fetch();
-					Navigation::getItem('/course')->setURL("/seminar_main.php?auswahl=". $result['seminar_id']);
-					Navigation::getItem('/course')->setTitle("Mein Kurs");
-				}
-				
-			}
-			
 			if (Navigation::hasItem('/start/my_courses')){
 			
 				if (Navigation::hasItem('/start/my_courses/browse')){
@@ -129,23 +69,14 @@ class VHSViewPlugin extends StudipPlugin implements SystemPlugin
 					Navigation::removeItem('/start/tools');
 				}
 				
-				$stmt = DBManager::get()->prepare("SELECT su.seminar_id FROM seminar_user su
-					WHERE su.user_id = ?");
-				$stmt->execute(array($GLOBALS['user']->id));
-				
-				$count = $stmt->rowCount();
-				if($count == 1){
-					$result = $stmt->fetch();
-					Navigation::getItem('/start/my_courses')->setURL("/seminar_main.php?auswahl=". $result['seminar_id']);
-					Navigation::getItem('/start/my_courses')->setTitle("Mein Kurs");
-				}
-				if($count == 0){
-					Navigation::removeItem('/start/my_courses');	
-				} 
-				if($count > 1){
-					Navigation::getItem('/start/my_courses')->setTitle("Kurse");
-				}
-				
+			}
+			
+			if (Navigation::hasItem('/schedule')){
+				Navigation::removeItem('/schedule');
+			}
+			
+			if (Navigation::hasItem('/community')){
+				Navigation::removeItem('/community');
 			}
 			
 			if (Navigation::hasItem('/start/community/browse')){
